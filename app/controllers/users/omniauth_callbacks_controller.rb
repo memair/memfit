6f9437cb04d965944f6a76e3cfb9d5898e3cc542
gregology <greg@gho.st>
2018-11-05 11:49:32 -0500

    auto redirect to google after authenticaion with memair

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,9 +3,10 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.from_memair_omniauth(request.env['omniauth.auth'])
 
     if @user.persisted?
-      flash[:notice] = 'Successfully connected with Memair, now connect with Google Fit'
+      flash[:notice] = 'Successfully connected with Memair, now connect Google Fit'
 
-      sign_in_and_redirect @user, event: :authentication
+      sign_in(:user, @user)
+      redirect_to user_google_oauth2_omniauth_authorize_path
     else
       session['devise.memair_data'] = request.env['omniauth.auth'].except(:extra) # Removing extra as it can overflow some session stores
       redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
@@ -14,7 +15,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def google_oauth2
     current_user.from_google_omniauth(request.env['omniauth.auth'])
-    flash[:success] = 'Successfully connected with Google Fit'
+    flash[:success] = 'Successfully connected with Memair with Google Fit'
     redirect_to root_path
   end
 end
